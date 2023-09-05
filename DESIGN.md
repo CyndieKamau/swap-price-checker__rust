9e0aabb58537ffd8b296bd9109a58cf64f345fef
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-09-05 14:44:31 +0300

    update logic for implementing ExchangeData

---
+++ DESIGN.md
@@ -0,0 +1,7 @@
+# Core functionalities of `ExchangeData`;
+
+* Store mock data about exchanges
+
+* Find out if a token swap exists on the exchange
+
+* Simulate a token swap on the exchange to know how much we will get in return
--- swap-price-checker/src/main.rs
+++ swap-price-checker/src/main.rs
@@ -32,6 +32,16 @@ enum Network {
    Polygon,   
 }
 
+//Error handling
+#[derive(Debug)]
+enum SwapError {
+
+    TokenPairNotSupported,
+    NotEnoughLiquidity,
+    InsufficientBalance,
+    IncorrectNetwork,
+}
+
 
 struct Token {
 
@@ -84,6 +94,50 @@ struct User {
 
 impl ExchangeData {
 
+
+    //checks if a token pair exists in the exchanges
+    pub fn check_token_pair(&self, from: TokenType, to: TokenType) -> Result<bool, SwapError> {
+
+        for pair in &self.token_pairs {
+
+            // If we find a match for both 'from' and 'to' tokens, pair is supported
+            if pair.from_token == from && pair.to_token == to {
+
+                return Ok(true);
+            }
+        }
+
+        //If loop completes without returning, the pair is not supported
+        Err(SwapError::TokenPairNotSupported)
+    }
+
+
+    //simulate the token swap
+    pub fn simulate_swap(&self, from: TokenType, to: TokenType, amount: f64) -> Result<f64, SwapError> {
+
+        // the "?" will return early with an Err if the pair isn't supported
+        self.check_token_pair(from, to)?       
+
+        for pair in &self.token_pairs {
+
+            if pair.from_token == from && pair.to_token == to {
+
+                if pair.liquidity as f64 >= amount {
+
+                    return Ok(amount * pair.swap_rate);
+                } else {
+
+                    return Err(SwapError::NotEnoughLiquidity);
+                }
+            }
+        }
+
+        //This is just a safety net for precaution
+        Err(SwapError::TokenPairNotSupported)
+    }
+
+
+   //Generate mock data for the exchanges
     pub fn mock_swap_data() -> Vec<Self> {
 
        let uniswap_pairs = vec![
@@ -188,11 +242,163 @@ impl ExchangeData {
            }
        ];
 
-       let cowswap_pairs = vec![];
+       //token pairs for CowSwap exchange
+       let cowswap_pairs = vec![
+       
+           TokenPairData {
+           
+               from_token: TokenType::USDT,
+               to_token: TokenType::USDC,
+               swap_rate: 1.003,
+               liquidity: 36_000_000,
+           },
 
-       let matcha_pairs = vec![];
+           TokenPairData {
 
-       let sushi_pairs = vec![];
+               from_token: TokenType::USDT,
+               to_token: TokenType::BUSD,
+               swap_rate: 1.0005,
+               liquidity: 32_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::USDC,
+               to_token: TokenType::USDT,
+               swap_rate: 0.997,
+               liquidity: 34_500_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::USDC,
+               to_token: TokenType::BUSD,
+               swap_rate: 1.0013,
+               liquidity: 30_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::BUSD,
+               to_token: TokenType::USDT,
+               swap_rate: 1.0009,
+               liquidity: 33_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::BUSD,
+               to_token: TokenType::USDC,
+               swap_rate: 0.9997,
+               liquidity: 29_000_000,
+           }
+       ];
+
+
+       //token pairs for Matcha Exchange
+       let matcha_pairs = vec![
+       
+           TokenPairData {
+           
+               from_token: TokenType::USDT,
+               to_token: TokenType::USDC,
+               swap_rate: 1.004,
+               liquidity: 42_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::USDT,
+               to_token: TokenType::BUSD,
+               swap_rate: 1.0006,
+               liquidity: 37_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::USDC,
+               to_token: TokenType::USDT,
+               swap_rate: 0.996,
+               liquidity: 40_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::USDC,
+               to_token: TokenType::BUSD,
+               swap_rate: 1.0011,
+               liquidity: 35_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::BUSD,
+               to_token: TokenType::USDT,
+               swap_rate: 1.0010,
+               liquidity: 38_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::BUSD,
+               to_token: TokenType::USDC,
+               swap_rate: 0.9995,
+               liquidity: 36_500_000,
+           }
+       ];
+
+
+       //token pairs for Sushi exchange
+       let sushi_pairs = vec![
+       
+           TokenPairData {
+           
+               from_token: TokenType::USDT,
+               to_token: TokenType::USDC,
+               swap_rate: 1.005,
+               liquidity: 48_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::USDT,
+               to_token: TokenType::BUSD,
+               swap_rate: 1.0007,
+               liquidity: 44_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::USDC,
+               to_token: TokenType::USDT,
+               swap_rate: 0.995,
+               liquidity: 46_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::USDC,
+               to_token: TokenType::BUSD,
+               swap_rate: 1.0010,
+               liquidity: 43_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::BUSD,
+               to_token: TokenType::USDT,
+               swap_rate: 1.0015,
+               liquidity: 45_000_000,
+           },
+
+           TokenPairData {
+
+               from_token: TokenType::BUSD,
+               to_token: TokenType::USDC,
+               swap_rate: 0.9998,
+               liquidity: 42_500_000,
+           }
+       ];
 
         vec![
 
